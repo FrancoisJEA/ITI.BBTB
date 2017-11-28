9f0df83b4cf892a2d2241fb746c968c131c0b4c6
JeanninF <fjeannin@intechinfo.fr>
2017-11-28 11:53:01 +0100

    Added serialization.

---
+++ ITI.BBTB.Final/BBTB.Tests/SimpleTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBTB.Tests
+{
+    [TestFixture]
+    public class SimpleTests
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.That(0, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ShouldFail()
+        {
+            Assert.That(0, Is.EqualTo(1));
+        }
+    }
+}
