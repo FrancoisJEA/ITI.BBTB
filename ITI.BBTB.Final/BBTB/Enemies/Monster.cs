919a06dc3adfeed9c556e0bf5c3a17312606a584
JeanninF <fjeannin@intechinfo.fr>
2017-12-04 15:03:43 +0100

    re Up monster bullet and Board

---
+++ ITI.BBTB.Final/BBTB/Enemies/Monster.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBTB
+{
+    public class Monster : Sprite
+    {
+        public bool IsAlive { get; set; }
+
+        public Monster(Texture2D texture, Vector2 position, SpriteBatch batch, bool isAlive)
+            : base(texture, position, batch)
+        {
+            IsAlive = isAlive;
+        }
+
+        public override void Draw()
+        {
+            if (IsAlive)
+            {
+                base.Draw();
+            }
+        }
+    }
+}
