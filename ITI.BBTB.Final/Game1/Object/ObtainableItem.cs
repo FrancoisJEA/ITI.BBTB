9c19246e0137814184b5938a9c8df1001da13fa7
MSI\Hichem <hichemdjamai2@gmail.com>
2017-12-06 10:41:25 +0100

    Inventory

---
+++ ITI.BBTB.Final/Game1/Object/ObtainableItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ 
+namespace Game1.Object
+{
+    public abstract class ObtainableItem
+        {
+            public Guid ID { get; set; }
+            public string Name { get; set; }
+            public int MaximumStackableQuantity { get; set; }
+
+
+            protected ObtainableItem()
+            {
+                MaximumStackableQuantity = 1;
+            }
+        }
+}
+
---
+++ ITI.BBTB.Final/Game1/Object/Potion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Object
+{
+    public class Potion : ObtainableItem
+    {
+        public Potion()
+        {
+            MaximumStackableQuantity = 5;
+        }
+    }
+}
