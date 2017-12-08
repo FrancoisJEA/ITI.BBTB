053c1a91ed1467fab5f4d5030e8b5f9e995729eb
JeanninF <fjeannin@intechinfo.fr>
2017-12-08 12:29:56 +0100

    Add HasTouchedEnemy and work

--- ITI.BBTB.Final/BBTB/Enemies/Monster.cs
+++ ITI.BBTB.Final/BBTB/Enemies/Monster.cs
@@ -11,6 +11,7 @@ namespace BBTB
     public class Monster : Sprite
     {
         public bool IsAlive { get; set; }
+        public int Life { get; set; }
 
         public Monster(Texture2D texture, Vector2 position, SpriteBatch batch, bool isAlive)
             : base(texture, position, batch)
