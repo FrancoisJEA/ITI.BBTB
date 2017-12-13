edfc22c3c7247f114dc9f16e53b3d1c387dda10f
Whyskit <b_atmans@hotmail.fr>
2017-12-13 10:22:20 +0100

    fixing bug

--- ITI.BBTB.Final/BBTB/Bullets/Bullet.cs
+++ ITI.BBTB.Final/BBTB/Bullets/Bullet.cs
@@ -10,7 +10,7 @@ namespace BBTB
 {
     public class Bullet : Sprite
     {
-        Board _ctx; 
+   
         float _rotation;
         Vector2 _origin;
         List<Monster> _enemys;
@@ -19,6 +19,7 @@ namespace BBTB
         public Bullet(Texture2D texture, Vector2 position, SpriteBatch spritebatch, WeaponLib ctx/*, List<Enemy> enemys*/)
             : base(texture, position, spritebatch)
         {
+            
             _origin = new Vector2(-27, 20);
             _rotation = ctx.Rotation;
             BulletLib = new BulletLib(ctx, new Vector2(base.position.X, base.position.Y), texture.Height, texture.Width);
@@ -31,7 +32,7 @@ namespace BBTB
         }
         public bool HasTouchedEnemy()
         {
-            foreach(Monster monster in _ctx.Monsters)
+            foreach(Monster monster in Board.CurrentBoard.Monsters)
             if (monster.IsAlive)
             {
                 return new Rectangle((int)position.X, (int)position.Y, Texture.Width, Texture.Height).Intersects(monster.Bounds);
