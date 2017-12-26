580cdb7db329f8264eccff98d8f3e897627043b3
JeanninF <fjeannin@intechinfo.fr>
2017-12-26 18:14:08 +0100

    first step of monster ia

--- ITI.BBTB.Final/BBTB/Enemies/Monster.cs
+++ ITI.BBTB.Final/BBTB/Enemies/Monster.cs
@@ -6,18 +6,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BBTB
 {
     public class Monster : Sprite
     {
         public bool IsAlive { get; set; }
         public int Life { get; set; }
+		Player player;
+		Vector2 _position;
+		double l;
+
 
         public Monster(Texture2D texture, Vector2 position, SpriteBatch batch, bool isAlive)
             : base(texture, position, batch)
         {
             IsAlive = isAlive;
         }
+
+		public void Intelligence()
+	    {
+			double k;
+			k = (position.X - player.position.X) * (position.X - player.position.X) + (position.Y - player.position.Y) * (position.Y - player.position.Y);
+			k = Math.Sqrt(k);
+			
+			if ( 10 < k && k <80 )
+			{
+				// this.shot(player.position);
+
+			}
+		}
+
+		// public static double Sqrt( double l) { return l;}
 
         public override void Draw()
         {
